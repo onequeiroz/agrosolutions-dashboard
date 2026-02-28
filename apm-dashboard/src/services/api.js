0a0f57b30aaf9732429cad08f5530553525d87dc
Gustavo Girotto Queiroz <gustavo.g.queiroz2015@gmail.com>
2026-02-27 21:58:03 -0300

    chamando as apis locais

--- apm-dashboard/src/services/api.js
+++ apm-dashboard/src/services/api.js
@@ -12,25 +12,20 @@ const api = axios.create({
   baseURL: "http://localhost:30080/api/monitoramento/",
 });
 
-// export const getSensorHistory = async (sensorId) => {
-//   const response = await api.get(`${sensorId}/metricas`);
-//   return response.data;
-// };
-
-// export const getAlerts = async (sensorId) => {
-//   const response = await api.get(`${sensorId}/alertas`);
-//   return response.data;
-// };
-
-// export const getSensorHistory = async (sensorId) => {
-//   const talhao = talhoes.find((t) => t.sensorId === sensorId);
-//   return fakeDelay(talhao.sensor);
-// };
-
 export const getSensorHistory = async (sensorId) => {
-  return responseMetrics.data;
+  const response = await api.get(`${sensorId}/metricas`);
+  return response.data;
 };
 
-export const getAlerts = async () => {
-  return alerts.data;
+export const getAlerts = async (sensorId) => {
+  const response = await api.get(`${sensorId}/alertas`);
+  return response.data;
 };
+
+// export const getSensorHistory = async (sensorId) => {
+//   return responseMetrics.data;
+// };
+
+// export const getAlerts = async () => {
+//   return alerts.data;
+// };
